4f7b9c4b5562251b8442f2c5568ca24b72cd5e60
Shane Sweeney <shanep.sween@gmail.com>
2021-03-09 13:00:44 -0800

    grid spacing cleanup

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,14 +3,16 @@ import Container from '@material-ui/core/Container'
 import {
   Avatar,
   Button,
+  Chip,
   Grid,
+  Hidden,
   makeStyles,
   Paper,
   Tooltip,
   Typography,
 } from '@material-ui/core'
-import CrossfadeImage from '../src/Crossfade/index'
-import { useTransition, animated, config } from 'react-spring'
+// import CrossfadeImage from '../src/Crossfade/index'
+// import { useTransition, animated, config } from 'react-spring'
 import PhonelinkRingIcon from '@material-ui/icons/PhonelinkRing'
 import EmailIcon from '@material-ui/icons/Email'
 import Head from 'next/head'
@@ -18,12 +20,12 @@ import Head from 'next/head'
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    paddingTop: theme.spacing(3),
+    padding: theme.spacing(3),
   },
   paper: {
-    padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+    padding: theme.spacing(2)
   },
   cardRoot: {
     width: '100%',
@@ -37,47 +39,76 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
     margin: theme.spacing(1),
+    '&.MuiChip-root': {
+      height: 48,
+      fontSize: '1rem',
+      '&.MuiChip-avatar': {
+        width: 40,
+        height: 40
+      }
+    },
+    '&.MuiChip-root .MuiChip-avatar': {
+      width: 40,
+      height: 40
+    }
   },
+  avatarProfile: {
+    width: theme.spacing(20),
+    height: theme.spacing(20),
+  },
+  avatarIcon: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  }
 }))
 
-const images = [
-  { id: 0, url: '/portrait.jpeg' },
-  { id: 2, url: '/golf.jpeg' },
-  { id: 3, url: '/mountain.jpeg' },
-  { id: 4, url: 'beer.jpeg' },
-]
+// const images = [
+//   { id: 0, url: '/portrait.jpeg' },
+//   { id: 2, url: '/golf.jpeg' },
+//   { id: 3, url: '/mountain.jpeg' },
+//   { id: 4, url: 'beer.jpeg' },
+// ]
 
-const icons = [
+const languageIcons = [
   { name: 'JavaScript', url: '/dev/js.svg' },
   { name: 'TypeScript', url: '/dev/typescript.svg' },
+  { name: 'C# (.NET)', url: '/dev/csharp.svg' },
+]
+
+const frontEndIcons = [
   { name: 'React', url: '/dev/react.svg' },
   { name: 'Redux', url: '/dev/redux.svg' },
+  { name: 'Material-UI', url: '/dev/materialui.svg' },
+]
+
+const backEndIcons = [
   { name: 'Nodejs', url: '/dev/nodejs.svg' },
   { name: 'Express', url: '/dev/express.svg' },
   { name: 'Sequelize', url: '/dev/sequelize.svg' },
+  { name: '.NET', url: '/dev/dot-net.svg' }
+]
+
+const databaseIcons = [
   { name: 'PostgreSQL', url: '/dev/postgresql.svg' },
-  { name: 'MS SQL Server', url: '/dev/microsoftsqlserver.svg' },
   { name: 'Firebase', url: '/dev/firebase.svg' },
-  { name: 'HTML5', url: '/dev/html5.svg' },
-  { name: 'Material-UI', url: '/dev/materialui.svg' },
-  { name: 'C# (.NET)', url: '/dev/csharp.svg' },
+  { name: 'MS SQL Server', url: '/dev/microsoftsqlserver.svg' },
 ]
 
 const About = () => {
   const classes = useStyles()
-  const [index, set] = useState(0)
+  // const [index, set] = useState(0)
 
-  const transitions = useTransition(images[index], (item) => item.id, {
-    from: { opacity: 0, position: 'absolute' },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
-    config: config.molasses,
-  })
+  // const transitions = useTransition(images[index], (item) => item.id, {
+  //   from: { opacity: 0, position: 'absolute' },
+  //   enter: { opacity: 1 },
+  //   leave: { opacity: 0 },
+  //   config: config.molasses,
+  // })
 
-  useEffect(
-    () => void setInterval(() => set((state) => (state + 1) % 4), 10000),
-    [set],
-  )
+  // useEffect(
+  //   () => void setInterval(() => set((state) => (state + 1) % 4), 10000),
+  //   [set],
+  // )
 
   return (
     <>
@@ -86,57 +117,104 @@ const About = () => {
       </Head>
       <Container className={classes.root}>
         <Paper className={classes.paper}>
-          <Grid container item xs spacing={3} direction="row">
-            <Grid item xs={4}>
-              <img src="portrait.jpeg" className={classes.image} />
+          <Grid container spacing={2}>
+            <Grid container item sm={4} xs={12} justify="center" >
+              <Grid item>
+                <Hidden only={["xs"]}>
+                  <img src="portrait.jpeg" className={classes.image} />
+                </Hidden>
+                <Hidden only={["xl", "lg", "md", "sm"]}>
+                  <Avatar className={classes.avatarProfile} src="portrait.jpeg" />
+                </Hidden>
+              </Grid>
             </Grid>
             <Grid
-              item
               container
-              xs={8}
+              item
+              sm={8}
               justify="center"
               alignItems="center"
+              spacing={3}
             >
               <Grid item>
                 <Typography variant="h6">
                   The name is Shane. I started my software journey in 2019.
                   I completed Fullstack Academy's immersive bootcamp in
-                  2020. I previously worked as a fullstack software
-                  developer with Awesemo. I am currently seeking roles in
-                  the Los Angeles area! In my free time, I enjoy being
-                  outdoors! Want to chat? Let's do it!
+                  2020. I previously worked as a fullstack
+                  developer with <a href="https://oddsshopper.awesemo.com/" target="_blank">Awesemo.com</a>. I am currently seeking roles in
+                  the Los Angeles area! <br /> Want to chat? Let's do it!
                     </Typography>
               </Grid>
               <Grid container item justify="center">
                 <Grid item>
                   {' '}
-                  <Button
-                    variant="contained"
+                  <Chip
+                    avatar={<Avatar><EmailIcon /></Avatar>}
+                    label="shanep.sween@gmail.com"
                     color="primary"
-                    size="large"
-                    target="_top"
-                    rel="noopener noreferrer"
-                    href={`mailto:shanep.sween@gmail.com`}
                     className={classes.button}
-                    startIcon={<EmailIcon />}
-                  >
-                    shanep.sween@gmail.com
-                      </Button>
+                  />
+
                 </Grid>
                 <Grid item>
-                  <Button
-                    variant="contained"
+                  <Chip
+                    avatar={<Avatar><PhonelinkRingIcon /></Avatar>}
+                    label="(773) 456-9892"
                     color="primary"
-                    size="large"
                     className={classes.button}
-                    startIcon={<PhonelinkRingIcon />}
-                  >
-                    (773) 456-9892
-                      </Button>
+                  />
                 </Grid>
               </Grid>
               <Grid container item justify="center" spacing={2}>
-                {icons.map((icon) => (
+                {languageIcons.map((icon) => (
+                  <Fragment key={icon.name}>
+                    <Grid item>
+                      <Tooltip title={icon.name}>
+                        <Avatar
+                          alt="icon"
+                          variant="square"
+                          sizes="lg"
+                          src={icon.url}
+                        />
+                      </Tooltip>
+                    </Grid>
+                  </Fragment>
+                ))}
+              </Grid>
+              <Grid container item justify="center" spacing={2}>
+                {frontEndIcons.map((icon) => (
+                  <Fragment key={icon.name}>
+                    <Grid item>
+                      <Tooltip title={icon.name}>
+                        <Avatar
+                          alt="icon"
+                          variant="square"
+                          sizes="lg"
+                          src={icon.url}
+                        />
+                      </Tooltip>
+                    </Grid>
+                  </Fragment>
+                ))}
+              </Grid>
+              <Grid container item justify="center" spacing={2}>
+                {backEndIcons.map((icon) => (
+                  <Fragment key={icon.name}>
+                    <Grid item>
+                      <Tooltip title={icon.name}>
+                        <Avatar
+                          alt="icon"
+                          variant="square"
+                          sizes="lg"
+                          src={icon.url}
+                        />
+                      </Tooltip>
+                    </Grid>
+                  </Fragment>
+                ))}
+              </Grid>
+              <Grid container item justify="center" spacing={2}>
+                {databaseIcons.map((icon) => (
                   <Fragment key={icon.name}>
                     <Grid item>
                       <Tooltip title={icon.name}>
